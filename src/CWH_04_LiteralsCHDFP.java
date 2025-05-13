9591cc1a333a458f9f6562bb7b2c2b6d1e4dc14a
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-05-13 19:21:16 +0530

    second commit

---
+++ src/CWH_04_LiteralsCHDFP.java
@@ -0,0 +1,46 @@
+
+/*
+integral data types -->byte ,short ,int ,long
+when we declared variable as long then we should write like long sal=122234556667L;
+then floatig-->float and double
+for float we should declare as float marks =88.0f,otherwise it will take it as double value
+for double its by defaultly its take as double its optional to declare like double marks=88.6d.
+then char -->we must declare in single quotes
+like char grade='A'.
+boolean -->true or false
+ */
+/*
+literals in java
+literals-->which are the constant values which assign to the variables
+examples
+101-->integer literals
+101.0f-->floating literals
+102.9-->doulbe literals
+true -->boolean literals
+"Harry"-->String literals
+'A'-->char literals
+ */
+public class CWH_04_LiteralsCHDFP {
+    public static void main(String []args)
+    {
+        byte age=34;
+        short age1=67;
+        int age3=89;
+        long bank_bal=7775566676776L;
+        float marks1=77.0f;
+        double marks2=99.0;
+        char grade='A';
+        boolean isTrue=true;
+        String name="Harry";
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age3);
+        System.out.println(bank_bal);
+        System.out.println(marks1);
+        System.out.println(marks2);
+        System.out.println(grade);
+        System.out.println(isTrue);
+        System.out.println(name);
+    }
+
+}
