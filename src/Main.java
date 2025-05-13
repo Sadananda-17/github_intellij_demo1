fba01a6068fa7048382d68a7ac9805f44a5b2a56
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-05-13 09:11:04 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello world");
+    }
+}
+// Basic structure of java program
+//Here public is a access modifier which decides the class accessebility
+// class is a keyword to create any classes
+// Main is a class name
+//static is a keyword where we  can run the program without creating any objects
+// void is a return data type which return nothin
+// main is a default method in java its a entry point to any java programs
+//String []args is a arguments
+
+
+/*
+Naming Convention in java
+Add two number
+AddTwoNumber-->Pascal naming convention used for names the classes
+addTwoNumber-->camelcase naming convention used for name the methods variables etc
+ */
+//comment in java
+//<--this is called singleline comment
+
+/* <--this is called multiline comments in java */
