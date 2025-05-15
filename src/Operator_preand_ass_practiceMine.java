48e55dfe7708a4564ef5db7476627d4088fac395
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-05-15 20:13:24 +0530

    practice programs

---
+++ src/Operator_preand_ass_practiceMine.java
@@ -0,0 +1,34 @@
+public class Operator_preand_ass_practiceMine {
+    public static void main(String[]args)
+    {
+        /*
+        int a=5;
+        int b=a++ + ++a;
+        System.out.println(b);
+        System.out.println();
+
+         */
+        /*
+        int result=10+2*3/2;
+        System.out.println("result"+result);
+
+         */
+        /*
+        int a,b,c;
+        a=b=c=20;
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
+        //here = is the assignment operator and its associativity goes right to left
+        so first c=20 then b=20,a=20;
+
+         */
+        /*
+        int a=2,b=3;
+        float result=(a+b)*(a-b);
+        System.out.println(result);
+
+         */
+
+    }
+}
