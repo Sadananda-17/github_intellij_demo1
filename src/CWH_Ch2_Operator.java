d0e2f2d23e24a82ea3f44c046c7f9994eb48212b
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-05-15 05:39:43 +0530

    operators in java

---
+++ src/CWH_Ch2_Operator.java
@@ -0,0 +1,61 @@
+
+//Operators in java
+
+public class CWH_Ch2_Operator {
+    public static void main(String []args)
+    {
+        /*
+
+        //1)Arithmetic Operator
+        int a=10,b=10;
+        System.out.println(a+b);//addition
+        System.out.println(a-b);//substraction
+        System.out.println(a*b);//muktiplication
+        System.out.println(a/b);//division
+        System.out.println(a%b);//modulo
+        float c=3.6f,d=1.2f;
+        System.out.println(c%d);//because the modulo operator works with the floating points/decimal values also
+
+        //2) Assignment Operators
+        int e=4,f=6;
+        System.out.println(e+=6);
+        System.out.println(e-=6);
+        System.out.println(e*=6);
+        System.out.println(e/=6);
+
+        //3) Comparission operators
+        int g=66,h=78;
+        System.out.println(h==g);
+        System.out.println(h>g);
+        System.out.println(h<g);
+        System.out.println(h>=g);
+        System.out.println(h<=g);
+        System.out.println(h!=g);
+
+        //4) logical Operator
+        System.out.println(1==1&&2==2);//AND
+        System.out.println(1==3&&2==2);
+        System.out.println(1==1||2==7);//OR
+        System.out.println(1!=0);//NOT
+
+         */
+
+
+
+        //5) bitwise operator
+        System.out.println(2&3);
+         /* explanation
+        2 in binary-->0010
+        3 in binary-->0011
+         bitwise and &----
+                        10
+         */
+        System.out.println(2|3);
+        /*
+        2 in binary 10
+        3 in binary 11
+        bitwise or |-----
+                     11
+         */
+    }
+}
