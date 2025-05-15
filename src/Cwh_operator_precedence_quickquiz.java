ced8cc7e0483626b513988ca254a3e6859271928
Sadananda-17 <sadananda17.s.shellikeri@gmail.com>
2025-05-15 20:10:05 +0530

    practice quiz

---
+++ src/Cwh_operator_precedence_quickquiz.java
@@ -0,0 +1,30 @@
+public class Cwh_operator_precedence_quickquiz {
+    public static void main(String[]args)
+    {
+        /*
+        int x=6,y=1;
+         int k=x-y/2;
+        System.out.println(k);
+
+         */
+        /*
+
+        int a=1,b=2,c=3;
+        int m=b*b-(4*a*c)/(2*a);
+        System.out.println(m);
+
+         */
+
+
+        /*
+        int v=10,u=10;
+        int n=v*v-u*u;
+        System.out.println(n);
+
+         */
+        int a=4,b=2,d=4;
+        int k=a*b-d;
+        System.out.println(k);
+
+    }
+}
